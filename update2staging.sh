9b6cbd2178dd8bc51623f2f3978e4468c2d32629
flash.g <flash.online@tuta.io>
2024-04-26 10:26:33 +0200

    new update skript

---
+++ update2staging.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+#Update OS
+sudo apt-get update
+sudo NEEDRESTART_MODE=a apt-get dist-upgrade -y
+
+#Get new code from the 'staging' branch
+sudo git -C /var/www/html fetch --all
+sudo git -C /var/www/html checkout staging
+sudo git -C /var/www/html pull origin staging
+
+#Install dependencies
+sudo COMPOSER_ALLOW_SUPERUSER=1 php /var/www/html/composer.phar -d /var/www/html --no-interaction install
+
+sudo chown -R www-data:www-data /var/www/html
+
+#Clear cache and update db
+sudo php /var/www/html/artisan cache:clear
+sudo php /var/www/html/artisan optimize
+sudo php /var/www/html/artisan migrate --force
+
+## Setup js
+sudo npm --prefix /var/www/html install
+#First dev, then prod to bake the keys into soketi(pusher)
+sudo npm --prefix /var/www/html run dev
+sudo npm --prefix /var/www/html run prod
+
+sudo chown -R www-data:www-data /var/www/html
+
+sudo systemctl restart artwork-worker
